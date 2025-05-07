f74c52ee600b75126c0af9b8bf35b6fb93d3bf44
Abdullah <abdullahehsan2251@gmail.com>
2025-05-07 16:57:00 +0500

    Api Created

--- chunks/chunks.go
+++ chunks/chunks.go
@@ -2,13 +2,25 @@ package chunks
 
 import (
 	"GOTASK/combo"
-	"fmt"
 )
 
-func Chunks(fileData string) {
+type chunkresult struct {
+	ChunkNumber       int
+	wordCount         int
+	Lines             int
+	Sentences         int
+	Paragraphs        int
+	Punctuations      int
+	Special_Character int
+	Vowels            int
+	Consonants        int
+	Digits            int
+}
+
+func Chunks(fileData string) []chunkresult {
 	chunksize := len(fileData) / 4
 	ch := make(chan []int)
-	var names = [10]string{"Word Count", "Lines", "Sentences", "Paragraphs", "Punctuations", "Special Characters", "Vowels", "Consonants", "Digits"}
+	//var names = [10]string{"Word Count", "Lines", "Sentences", "Paragraphs", "Punctuations", "Special Characters", "Vowels", "Consonants", "Digits"}
 
 	chunk := []string{
 		fileData[:chunksize],
@@ -16,16 +28,25 @@ func Chunks(fileData string) {
 		fileData[chunksize*2 : chunksize*3],
 		fileData[chunksize*3:],
 	}
+	var result []chunkresult
 	for i := 0; i < 4; i++ {
-		
-		go combo.Combo(chunk[i], ch)
-		fmt.Printf("\n")
-		fmt.Println("Chunk No:", i)
-		value := <-ch
-		for j := 0; j < 9; j++ {
-			fmt.Println(names[j], value[j])
 
-		}
+		go combo.Combo(chunk[i], ch)
+		value := <-ch
+		result = append(result, chunkresult{
+			ChunkNumber:       i + 1,
+			wordCount:         value[0],
+			Lines:             value[1],
+			Sentences:         value[2],
+			Paragraphs:        value[3],
+			Punctuations:      value[4],
+			Special_Character: value[5],
+			Vowels:            value[6],
+			Consonants:        value[7],
+			Digits:            value[8],
+		})
+
 	}
+	return result
 
 }
--- main.go
+++ main.go
@@ -3,54 +3,93 @@ package main
 import (
 	"GOTASK/chunks"
 	"GOTASK/combo"
-	"GOTASK/separate"
 	"fmt"
-	"os"
-	"sync"
+	"io"
+	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 	fmt.Println("Reading File")
-	textfile := "Text.txt"
-	FileData, err := os.ReadFile(textfile)
-	if err != nil {
-		panic(err)
-	}
-	Str := string(FileData)
-	startTime := time.Now()
-	var wg sync.WaitGroup
-	wg.Add(9)
-	go separate.WordCounter(Str, &wg)
-	go separate.LinesCounter(Str, &wg)
-	go separate.SentenceCounter(Str, &wg)
-	go separate.ParasCounter(Str, &wg)
-	go separate.PuncCounter(Str, &wg)
-	go separate.SpecialCounter(Str, &wg)
-	go separate.VowelsCounter(Str, &wg)
-	go separate.ConsonantsCounter(Str, &wg)
-	go separate.DigitsCounter(Str, &wg)
-	wg.Wait()
-	timeTaken := time.Since(startTime)
-	fmt.Println("Time Taken After Using Goroutine: ", timeTaken)
-	fmt.Printf("\n")
+	router := gin.Default()
+	router.POST("/analyze", func(ctx *gin.Context) {
+		file, err := ctx.FormFile("sample")
+		if err != nil {
+			ctx.String(http.StatusBadRequest, "error Reading File: %v", err)
+		}
+		file2, _ := file.Open()
+		defer file2.Close()
+		FileData, _ := io.ReadAll(file2)
 
-	startTime2 := time.Now()
-	ch := make(chan []int)
-	var names = [9]string{"Word Count", "Lines", "Sentences", "Paragraphs", "Punctuations", "Special Characters", "Vowels", "Consonants", "Digits"}
-	go combo.Combo(Str, ch)
-	value := <-ch
-	for i := 0; i < 9; i++ {
+		Str := string(FileData)
+		// startTime := time.Now()
+		// var wg sync.WaitGroup
+		// wg.Add(9)
+		// go separate.WordCounter(Str, &wg)
+		// go separate.LinesCounter(Str, &wg)
+		// go separate.SentenceCounter(Str, &wg)
+		// go separate.ParasCounter(Str, &wg)
+		// go separate.PuncCounter(Str, &wg)
+		// go separate.SpecialCounter(Str, &wg)
+		// go separate.VowelsCounter(Str, &wg)
+		// go separate.ConsonantsCounter(Str, &wg)
+		// go separate.DigitsCounter(Str, &wg)
+		// wg.Wait()
+		// timeTaken := time.Since(startTime)
+		// fmt.Println("Time Taken After Using Goroutine: ", timeTaken)
+		// fmt.Printf("\n")
 
-		fmt.Println(names[i], value[i])
+		startTime2 := time.Now()
+		ch := make(chan []int)
+		// //var names = [9]string{"Word Count", "Lines", "Sentences", "Paragraphs", "Punctuations", "Special Characters", "Vowels", "Consonants", "Digits"}
+		go combo.Combo(Str, ch)
+		value := <-ch
+		timeTaken2 := time.Since(startTime2)
+		// ctx.JSON(200, gin.H{
+		// 	"Filename":     file.Filename,
+		// 	"File Size":    file.Size,
+		// 	"Word Count ":  value[0],
+		// 	"Lines ":       value[1],
+		// 	"sentences":    value[2],
+		// 	"paragraphs":   value[3],
+		// 	"punctuations": value[4],
+		// 	"specialChar":  value[5],
+		// 	"vowelscount":  value[6],
+		// 	"consonants":   value[7],
+		// 	"digits":       value[8],
+		// 	"Time Taking: " : timeTaken2,
+		// })
 
-	}
-	timeTaken2 := time.Since(startTime2)
-	fmt.Println("Time Taken After Using Channels & Goroutines: ", timeTaken2)
+		// for i := 0; i < 9; i++ {
 
-	startTime3 := time.Now()
-	chunks.Chunks(Str)
-	timeTaken3 := time.Since(startTime3)
-	fmt.Println("Time Taken After Using Chunks: ", timeTaken3)
+		// 	fmt.Println(names[i], value[i])
+
+		// }
+
+		//	fmt.Println("Time Taken After Using Channels & Goroutines: ", timeTaken2)
+
+		startTime3 := time.Now()
+		result := chunks.Chunks(Str)
+		timeTaken3 := time.Since(startTime3)
+		ctx.JSON(200, gin.H{
+			"Filename":                                file.Filename,
+			"Using Combination Function Word Count ":  value[0],
+			"Using Combination Function Lines ":       value[1],
+			"Using Combination Function sentences":    value[2],
+			"Using Combination Function paragraphs":   value[3],
+			"Using Combination Function punctuations": value[4],
+			"Using Combination Function specialChar":  value[5],
+			"Using Combination Function vowelscount":  value[6],
+			"Using Combination Function consonants":   value[7],
+			"Using Combination Function digits":       value[8],
+			"Time Taken for Combination Function: ":   timeTaken2.String(),
+			"Chunk Result":                            result,
+			"Time Taken for Chunks ":                  timeTaken3.String(),
+		})
+
+	})
+	router.Run(":8080")
 }
